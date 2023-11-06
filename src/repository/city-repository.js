0b5b583a63ad98709102016a433c86ddc89f6b31
Mohit Joshi <b210055@nitsikkim.ac.in>
2023-11-06 18:32:30 +0530

    Completed the CityRepository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -4,23 +4,60 @@ const {City} = require('../models/index');
 class CityRepository{
     async createCity({name}){
         try{
-            const city = await City.create({name});
-        } catch(error){
+            const city = await City.create({
+                name
+            });
+            return city;
+        }catch(error){
+            console.log("Something went wrong in the repository layer");
             throw(error);
         }
     }
 
     async deleteCity({cityId}){
         try{
-            await City.destroy({
+            City.destroy({
                 where: {
                     id: cityId
                 }
             });
-        } catch(error){
+            return true;
+        }catch(error){
+            console.log("Something went wrong in the repository layer");
             throw(error);
         }
     }
+
+    async updateCity(cityId, data){
+        try {
+            const city = await City.update(data,{
+                where:{
+                    id:cityId
+                }
+            })
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw(error);
+        }
+    }
+
+
+    async getCity(cityId){                    
+        try{
+            // const city = await City.findOne({           //As city id is a primary key, so we can use findByPk(fin by primary key) to directly get the details
+            // where: {
+            //     id: cityId
+            // }
+            const city = await City.findByPk(cityId);
+            return city;
+        } catch(error){
+            console.log("Something went wrong in the repository layer");
+            throw(error);
+        }
+    }
+
+
 }
 
 module.exports = CityRepository;
