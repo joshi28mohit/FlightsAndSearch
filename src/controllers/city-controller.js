4e21caf8e15d81fe797f9f8fb0d54d3e912d7a38
Mohit Joshi <b210055@nitsikkim.ac.in>
2023-11-06 23:07:50 +0530

    Started writing the controller files

---
+++ src/controllers/city-controller.js
@@ -0,0 +1,40 @@
+const { CityService } = require('../services/index');
+
+const create = (req,res) => {
+    try {
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const destroy = (req,res) => {
+    try {
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const get = (req,res) => {
+    try {
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const update = (req,res) => {
+    try {
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = {
+    create,
+    update,
+    destroy,
+    get
+}
